7ad452781831e8e1898915fde0b81c6d6ffeff7e
Felipe Richter <frichterc@gmail.com>
2024-07-11 18:43:29 -0400

    base project

---
+++ src/test/java/cl/devopcitos/alertasismos/AlertaSismosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.devopcitos.alertasismos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertaSismosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
