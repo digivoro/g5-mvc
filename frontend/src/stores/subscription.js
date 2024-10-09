e9b9e0e7c6ad85ca5a563ccd210b7104f68fffe2
Felipe Richter <frichterc@gmail.com>
2024-10-09 14:24:47 -0300

    feat: agrego store de suscripciones

---
+++ frontend/src/stores/subscription.js
@@ -0,0 +1,28 @@
+import { defineStore } from "pinia";
+
+export const useSubscriptionStore = defineStore("subscriptionStore", () => {
+  const subscriptions = ref([]);
+  const isLoading = ref(false);
+
+  async function getSubscriptions() {
+    try {
+      isLoading.value = true;
+      const BASE_URL =
+        import.meta.env.VITE_API_URL || "http://localhost:8080/api";
+      const URL = BASE_URL + "/suscripciones";
+      const res = await fetch(URL);
+      const data = await res.json();
+      subscriptions.value = [...data];
+    } catch (error) {
+      console.error(error);
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  return {
+    subscriptions,
+    isLoading,
+    getSubscriptions,
+  };
+});
