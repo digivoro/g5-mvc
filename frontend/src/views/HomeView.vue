b23b2a4ff2968e1c784d2c083fd162ccc1d662b7
Felipe Richter <frichterc@gmail.com>
2024-10-08 21:09:02 -0300

    feat: configuro tailwind

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <h2 class="text-xl bg-red-500">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut molestias
+      labore quidem eos architecto ipsa error incidunt eveniet iure ipsam.
+      Nostrum facilis nihil similique dolorem quos, aspernatur iusto ducimus
+      nobis! Saepe, aut expedita ratione possimus veniam quis dicta deleniti
+      ipsam voluptatum nesciunt ipsum iusto aperiam provident eligendi! Nemo
+      tempora qui, corrupti reprehenderit minima, magni illum expedita deserunt,
+      molestias dignissimos corporis.
+    </h2>
   </main>
 </template>
