384467d0c352e6ebd0c788465f3c95637c343dee
Felipe Richter <frichterc@gmail.com>
2024-10-09 02:02:23 -0300

    feat: agrego excepcion cors para desarrollo local

--- backend/src/main/java/cl/devopcitos/alertasismos/AlertaSismosApplication.java
+++ backend/src/main/java/cl/devopcitos/alertasismos/AlertaSismosApplication.java
@@ -9,5 +9,4 @@ public class AlertaSismosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AlertaSismosApplication.class, args);
 	}
-
 }
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO sismo (localidad, fecha, profundidad, magnitud) VALUES ('Panguipulli', '2024-08-30', 112, 3.1);
+INSERT INTO sismo (localidad, fecha, profundidad, magnitud) VALUES ('Pomaire', '2024-10-09', 45, 3.7);
+INSERT INTO sismo (localidad, fecha, profundidad, magnitud) VALUES ('Pichilemu', '2024-10-04', 20, 6.6);
