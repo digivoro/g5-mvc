f12d3449c021d985a8c42824f903ab5bce993c8a
Felipe Richter <frichterc@gmail.com>
2024-10-09 14:20:34 -0300

    feat: agrego ruta suscripciones

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,12 +6,13 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: import("../views/HomeView.vue"),
     },
     {
-      path: "/about",
-      name: "about",
-      component: () => import("../views/AboutView.vue"),
+      path: "/suscripciones",
+      alias: "/subscriptions",
+      name: "subscriptions",
+      component: () => import("../views/SubscriptionsView.vue"),
     },
   ],
 });
---
+++ frontend/src/views/SubscriptionsView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-xl">Suscripciones</h1>
+  </div>
+</template>
