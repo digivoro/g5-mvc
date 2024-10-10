fb2a4235fead3ba492db71b7a3fa987840078b62
Felipe Richter <frichterc@gmail.com>
2024-10-10 13:58:44 -0300

    feat: agrego modelo, controladores, repositorio y servicio para suscripciones

--- backend/src/main/java/cl/devopcitos/alertasismos/controllers/SuscripcionController.java
+++ backend/src/main/java/cl/devopcitos/alertasismos/controllers/SuscripcionController.java
@@ -1,6 +1,44 @@
 package cl.devopcitos.alertasismos.controllers;
 
+import cl.devopcitos.alertasismos.models.Suscripcion;
+import cl.devopcitos.alertasismos.services.SuscripcionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+@RestController
+@RequestMapping("/api/suscripciones")
 public class SuscripcionController {
-  
+  @Autowired
+  private SuscripcionService suscripcionService;
+
+  @GetMapping
+  public List<Suscripcion> getAllSuscripcions(){
+    return suscripcionService.getAllSuscripcions();
+  }
+
+  @PostMapping
+  public Suscripcion createSuscripcion(@RequestBody Suscripcion suscripcion){
+    return suscripcionService.createSuscripcion(suscripcion);
+  }
+
+  @GetMapping("/{id}")
+  public Suscripcion oneSuscripcion(@PathVariable Long id){
+    return suscripcionService.getSuscripcionById(id);
+  }
+
+  @PutMapping("/{id}")
+  public Suscripcion updateSuscripcion(@PathVariable Long id, @RequestBody Suscripcion suscripcion){
+    Suscripcion updatedSuscripcion = suscripcionService.getSuscripcionById(id);
+    updatedSuscripcion.setLocalidad(suscripcion.getLocalidad());
+    updatedSuscripcion.setEmail(suscripcion.getEmail());
+    updatedSuscripcion.setNombre(suscripcion.getNombre());
+    return suscripcionService.createSuscripcion(updatedSuscripcion);
+  }
+
+  @DeleteMapping("/{id}")
+  public boolean deleteSuscripcion(@PathVariable Long id){
+    suscripcionService.deleteSuscripcion(id);
+    return true;
+  }
 }
---
+++ backend/src/main/java/cl/devopcitos/alertasismos/models/Suscripcion.java
@@ -0,0 +1,86 @@
+package cl.devopcitos.alertasismos.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="suscripcion")
+public class Suscripcion {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column
+  private String localidad;
+  private String email;
+  private String nombre;
+  private Boolean activo;
+
+  public Suscripcion() {}
+
+  public Suscripcion(String localidad, String email, String nombre, Boolean activo) {
+    this.localidad = localidad;
+    this.email = email;
+    this.nombre = nombre;
+    this.activo = true;
+  }
+
+  public Suscripcion(Long id, String localidad, String email, String nombre, Boolean activo) {
+    this.id = id;
+    this.localidad = localidad;
+    this.email = email;
+    this.nombre = nombre;
+    this.activo = true;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+  
+  public String getLocalidad() {
+    return localidad;
+  }
+
+  public void setLocalidad(String localidad) {
+    this.localidad = localidad;
+  }
+  
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Boolean getActivo() {
+    return activo;
+  }
+
+  public void setActivo(Boolean activo) {
+    this.activo = activo;
+  }
+
+  @Override
+  public String toString() {
+    return "Sismo{" +
+                "id=" + id +
+                ", localidad='" + localidad + '\'' +
+                ", email=" + email +
+                ", nombre=" + nombre +
+                ", estado=" + activo +
+                '}';
+  }
+
+}
