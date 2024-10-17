2e8b3d34b07a6eb66ca8c642819f990b3379129f
DiegoAM7 <diiego.am7@gmail.com>
2024-10-17 18:32:29 -0300

    docker containers

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,20 +1,20 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: import("../views/HomeView.vue"),
-    },
-    {
-      path: "/suscripciones",
-      alias: "/subscriptions",
-      name: "subscriptions",
-      component: () => import("../views/SubscriptionsView.vue"),
-    },
-  ],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: () => import('../views/HomeView.vue'),
+		},
+		{
+			path: '/suscripciones',
+			alias: '/subscriptions',
+			name: 'subscriptions',
+			component: () => import('../views/SubscriptionsView.vue'),
+		},
+	],
 });
 
 export default router;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,16 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-    fontFamily: {
-      logo: ["Yeseva One"],
-    },
-  },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["synthwave"],
-  },
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+		fontFamily: {
+			logo: ['Yeseva One'],
+		},
+	},
+	plugins: [daisyui],
+	daisyui: {
+		themes: ['synthwave'],
+	},
 };
